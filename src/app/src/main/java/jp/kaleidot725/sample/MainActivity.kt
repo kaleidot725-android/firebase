b7dbde7ee92a3f0fbc0155994b43a2cc5c21ed7d
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-04-02 23:24:19 +0900

    Add Sample Code

--- src/app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
+++ src/app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
@@ -1,12 +1,31 @@
 package jp.kaleidot725.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = FirebaseFirestore.getInstance()
+        val user = hashMapOf("first" to "Ada", "last" to "Lovelace", "born" to 1815)
+
+        db.collection("users").add(user).addOnSuccessListener { documentReference ->
+            Log.d("MainActivity", "DocumentSnapshot added with ID: ${documentReference.id}")
+        }.addOnFailureListener { e ->
+            Log.w("MainActivity", "Error adding document", e)
+        }
+
+        db.collection("users").get().addOnSuccessListener { result ->
+            for (document in result) {
+                Log.d("MainActivity", "${document.id} => ${document.data}")
+            }
+        }.addOnFailureListener { exception ->
+            Log.w("MainActivity", "Error getting documents.", exception)
+        }
     }
 }
